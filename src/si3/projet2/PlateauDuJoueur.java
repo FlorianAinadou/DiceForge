f5e281588c831ab94215f4c968661113b5bd168d
Florian <florian.ainadou97@gmail.com>
2018-10-21 22:22:18 +0200

    Modification de la classe Plateau du joueur

--- src/si3/projet2/PlateauDuJoueur.java
+++ src/si3/projet2/PlateauDuJoueur.java
@@ -1,5 +1,66 @@
 package si3.projet2;
 
+
+/**
+*Cette classe représente le plateau d'un joueur de Dice Forge.
+ */
 public class PlateauDuJoueur {
+
+        //l'Identifiant du joueur concerné.
+        private int idJoueur;
+        //Les points de gloire du joueur concerné.
+        private int pointsDeGloire;
+
+
+    /**
+     * Crée un plateau qui rassemble les informations correspondantes à un joueur
+     *
+     * @param idJoueur identifiant du joueur concerné.
+     * @param pointsDeGloire ponits de goire du joueur concerné.
+     */
+
+
+
+    PlateauDuJoueur(int idJoueur, int pointsDeGloire){
+        this.pointsDeGloire=pointsDeGloire;
+        this.idJoueur=idJoueur;
+    }
+
+    PlateauDuJoueur(int idJoueur){
+        this.pointsDeGloire=0;
+        this.idJoueur=idJoueur;
+    }
+
+    /**
+     * Retourne les points de gloire du joueur
+     */
+
+    public int getPointsDeGloire() {
+        return pointsDeGloire;
+    }
+
+    /**
+     * Retourne l'identifiant du joueur
+     */
+    public int getIdJoueur(){
+        return idJoueur;
+    }
+
+    /**
+     * Permet de modifier les points de gloire du joueur
+     * @param pointsDeGloire
+     */
+    public void setPointsDeGloire(int pointsDeGloire) {
+        this.pointsDeGloire = pointsDeGloire;
+    }
+
+    /**
+     * permet de définir le joueur concerné
+     * @param idJoueur
+     */
+    public void setJoueur(int idJoueur) {
+        this.idJoueur = idJoueur;
+    }
+
 
 }
