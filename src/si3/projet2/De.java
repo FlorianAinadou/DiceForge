7385f202e1e96c129960a28377fa3756252d8a18
Youssef Mekouar <youssef.mekouar@etu.unice.fr>
2018-10-23 00:19:45 +0200

    codage : Class Face + Class Dé #2

--- src/si3/projet2/De.java
+++ src/si3/projet2/De.java
@@ -1,4 +1,64 @@
 package si3.projet2;
 
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+/**
+* Cette classe représente les dés dans Dice Forge
+ */
 public class De {
+    //C'est un tableau d'objet de type Face qui regroupe les faces du dé.
+    private Face[] de;
+    // C'est un int qui définit le nombre de face d'un dé
+    private int nbFace = 6;
+
+    /**
+     * Crée un dé qui regroupe des faces et initialise les faces du dé par défault (nb à 0 et typeDeRessource à gloire)
+     * @param aucun
+     */
+
+    public De(){
+        this.de = new Face[nbFace];
+        for(int i =0;i<6;i++){
+            this.de[i]= new Face(0,"gloire");
+        }
+    }
+
+    /**
+     * Cette méthode nous permet d'ajouter une face dans n'importe qu'elle face du dé
+     * @param f --> face à ajouter
+     * @param indice --> quelle face du dé
+     */
+    public void ajouterUneFace(Face f, int indice){
+        this.de[indice]= f;
+
+    }
+
+    public void ajouterToutLesFace(ArrayList list){
+
+    }
+
+    /**
+     * Cette méthode simule un lancement de dé
+     * @return cette méthode retourne la face du dé qui a était lancé.
+     */
+    public Face lancerLeDe(){
+        Random r = new Random();
+        int valeur = r.nextInt(5);
+        return this.de[valeur];
+    }
+
+    /**
+     * Cette méthode affiche les faces d'un dé
+     */
+    public void afficherDe(){
+        for(int i=0 ; i<6;i++){
+            System.out.println("Val : " + de[i].getNb()+ " type :" + de[i].getTypeDeRessource() );
+        }
+    }
+
+
+
 }
--- src/si3/projet2/Face.java
+++ src/si3/projet2/Face.java
@@ -1,6 +1,40 @@
 package si3.projet2;
 
+/**
+ * Cette classe représente une face d'un dé
+ */
 public class Face {
+    private int nb;
+    private String typeDeRessource;
+
+    /**
+     * Ce constructeur cré une face caractérisée par:
+     * @param nb
+     * @param typeDeRessource
+     */
+    public Face(int nb , String typeDeRessource){
+        this.nb=nb;
+        this.typeDeRessource=typeDeRessource;
+    }
+
+    /**
+     * @return le nombre de ressource gagner
+     */
+    public int getNb() {
+        return nb;
+    }
+
+    /**
+     *
+     * @return le type de ressource gagner
+     */
+    public String getTypeDeRessource() {
+        return typeDeRessource;
+    }
+
+
+
+
 
 }
 
