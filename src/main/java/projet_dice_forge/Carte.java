5056178cc7f87f5e953a890bad6ef3c2491d20c8
Unknown <bast.jard@gmail.com>
2018-11-14 16:46:34 +0100

    Ajout des cartes dans les îles

--- src/main/java/projet_dice_forge/Carte.java
+++ src/main/java/projet_dice_forge/Carte.java
@@ -1,14 +1,36 @@
 package main.java.projet_dice_forge;
 
+import java.util.HashMap;
+
 public class Carte {
-    String typeCarte;
+    private String nomDeLexploit;
+    private int[] prix;
+    private String[] Ressource;
+    private int pointDeGloire;
+    private int idCarte;
 
-
-    public Carte(String typeCarte){
-        this.typeCarte=typeCarte;
+    public Carte(int idCarte,String nomDeLexploit,int[] prix, String[] Ressource, int pointDeGloire){
+        this.nomDeLexploit=nomDeLexploit;
+        this.prix = prix;
+        this.Ressource = Ressource;
+        this.pointDeGloire=pointDeGloire;
+        this.idCarte=idCarte;
     }
 
     public void effetCarte(){
 
+    }
+    public void afficherCarte(){
+        System.out.println(this.nomDeLexploit);
+    }
+
+    public int getPrix(int i){
+        return this.prix[i];
+    }
+    public String getRessource(int i) {
+        return this.Ressource[i];
+    }
+    public int getPointDeGloire(){
+        return this.pointDeGloire;
     }
 }
