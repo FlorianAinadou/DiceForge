0fa98a2880ee2569d532dce11e25d6a291539298
Florian <florian.ainadou97@gmail.com>
2018-11-27 13:44:22 +0100

    Première opération du bot
    Le bot Emeline sait faire des choix par rapport aux faces à acheter dans le bassin

--- src/main/java/projet_dice_forge/Bot/Emeline.java
+++ src/main/java/projet_dice_forge/Bot/Emeline.java
@@ -2,6 +2,9 @@ package main.java.projet_dice_forge.Bot;
 
 import main.java.projet_dice_forge.Partie_Bassin.Temple;
 import main.java.projet_dice_forge.Partie_Iles.PlateauDesIles;
+import main.java.projet_dice_forge.Plateau_Joueur.Face;
+
+import java.util.ArrayList;
 
 public class Emeline extends Bot {
 
@@ -11,8 +14,49 @@ public class Emeline extends Bot {
 
     @Override
     public void jouer(Temple temple, PlateauDesIles plateauDesIles) {
-        super.jouer(temple, plateauDesIles);
+            super.jouer(temple, plateauDesIles);
+
+            ArrayList<Face> facesDisponibles = new ArrayList<>();
+
+
+            int prixDuMeilleurPlateau =this.quelEstLeMeilleurBassin(temple);
+
+            switch (prixDuMeilleurPlateau) {
+                case 2: {
+                    facesDisponibles = temple.getFaceFromBassin(2);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+                case 3: {
+                    facesDisponibles = temple.getFaceFromBassin(3);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+                case 4: {
+                    facesDisponibles = temple.getFaceFromBassin(4);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+                case 5:{
+                    facesDisponibles = temple.getFaceFromBassin(5);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+                case 6:{
+                    facesDisponibles = temple.getFaceFromBassin(6);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+                case 8:{
+                    facesDisponibles = temple.getFaceFromBassin(8);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+                case 12:{
+                    facesDisponibles = temple.getFaceFromBassin(12);
+                    this.acheterFace(temple, facesDisponibles.get(1), new Face(new int[]{1}, new String[]{"Or"}), 1);
+                }
+
+            }
+
+        }
+
+
 
     }
 
-}
+
