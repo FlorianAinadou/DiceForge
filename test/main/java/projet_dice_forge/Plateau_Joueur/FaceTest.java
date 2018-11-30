4b7532ca44b37a901bc8280190a5d94966c36015
Florian <florian.ainadou97@gmail.com>
2018-11-30 02:04:32 +0100

    Test sur le Face

---
+++ test/main/java/projet_dice_forge/Plateau_Joueur/FaceTest.java
@@ -0,0 +1,35 @@
+package main.java.projet_dice_forge.Plateau_Joueur;
+
+import main.java.projet_dice_forge.Ressource.FragementLunaire;
+import main.java.projet_dice_forge.Ressource.FragementSolaire;
+import main.java.projet_dice_forge.Ressource.Or;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FaceTest {
+
+
+    @Test
+    void ajouterRessource() {
+        Face face= new Face();
+        face.AjouterRessource(new Or(2));
+        assertNotNull(face.getRessource());
+    }
+
+    @Test
+    void afficherFace() {
+        Face face= new Face();
+        face.AjouterRessource(new Or(2));
+        assertEquals("2 Or \n", face.afficherFace());
+    }
+
+    @Test
+    void getValeurFace() {
+        Face face= new Face();
+        face.AjouterRessource(new Or(2));
+        face.AjouterRessource(new FragementLunaire(2));
+        face.AjouterRessource(new FragementSolaire(1));
+        assertEquals(5,face.getValeurFace());
+    }
+}
